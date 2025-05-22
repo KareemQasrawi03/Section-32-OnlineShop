6704b6411e4c87278a7a41e0240e154d559af0ef
kajamal <kajamal@ejada.com>
2025-05-22 11:38:30 +0300

    install ejs package and add head, header and footer

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const path = require("path")
 const express = require("express")
 const app = express()
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,"views"))
 
 const authRoutes = require("./routes/auth.routes")
 
