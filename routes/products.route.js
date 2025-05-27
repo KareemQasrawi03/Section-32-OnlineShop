2e191c473564ab01e62acd95bf56833b32048e3b
kajamal <kajamal@ejada.com>
2025-05-27 12:27:47 +0300

    Outputting Products For Customers

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const productsController = require("../controllers/products.controllers");
 
 const router = express.Router();
 
-router.get("/products", function (req, res) {
-  res.render("customer/products/all-products");
-});
+router.get("/products", productsController.getAllProducts);
 
 module.exports = router;
